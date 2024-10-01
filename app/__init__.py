52d1910a21a48a972b52372aa816d9de20bb374b
dan <125440936+dwreckTheOG@users.noreply.github.com>
2024-10-01 16:04:09 +0300

    database initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from config import Config
+from app.models import *
+from app.extensions import db
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
     return app
